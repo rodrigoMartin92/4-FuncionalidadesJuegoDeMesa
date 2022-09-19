1c48945444b2d402ec809307aa97f245f44fbb1c
Rodrigo <rodmarmus@gmail.com>
2022-09-19 02:10:33 -0300

    Funcionalidad 1 de dados si funciona

--- dados.js
+++ dados.js
@@ -4,31 +4,44 @@ var dado3Atk = 0
 var dado1Def = 0
 var dado2Def = 0
 var dado3Def = 0
-
+console.log("000 - dado1Atk = " + dado1Atk + " - dado2Atk = " + dado2Atk + " - dado3Atk = " + dado3Atk + " - dado1Def = " + dado1Def + " - dado2Def = " + dado2Def + " - dado3Def = " + dado3Def)
 
 function lanzarDadoAtaque() {
-    var dado1Atk = Math.floor(Math.random() * 6) + 1;
+    dado1Atk = Math.floor(Math.random() * 6) + 1;
     document.getElementById("idDado1").innerHTML = dado1Atk;
-    var dado2Atk = Math.floor(Math.random() * 6) + 1;
+    dado2Atk = Math.floor(Math.random() * 6) + 1;
     document.getElementById("idDado2").innerHTML = dado2Atk;
-    var dado3Atk = Math.floor(Math.random() * 6) + 1;
+    dado3Atk = Math.floor(Math.random() * 6) + 1;
     document.getElementById("idDado3").innerHTML = dado3Atk;
+    console.log("AAA - dado1Atk = " + dado1Atk + " - dado2Atk = " + dado2Atk + " - dado3Atk = " + dado3Atk + " - dado1Def = " + dado1Def + " - dado2Def = " + dado2Def + " - dado3Def = " + dado3Def)
 }
 function lanzarDadoDefensa() {
-    var dado1Def = Math.floor(Math.random() * 6) + 1;
+    dado1Def = Math.floor(Math.random() * 6) + 1;
     document.getElementById("idDado4").innerHTML = dado1Def;
-    var dado2Def = Math.floor(Math.random() * 6) + 1;
+    dado2Def = Math.floor(Math.random() * 6) + 1;
     document.getElementById("idDado5").innerHTML = dado2Def;
-    var dado3Def = Math.floor(Math.random() * 6) + 1;
+    dado3Def = Math.floor(Math.random() * 6) + 1;
     document.getElementById("idDado6").innerHTML = dado3Def;
+    console.log("BBB - dado1Atk = " + dado1Atk + " - dado2Atk = " + dado2Atk + " - dado3Atk = " + dado3Atk + " - dado1Def = " + dado1Def + " - dado2Def = " + dado2Def + " - dado3Def = " + dado3Def)
 }
 function compararDados() {
-    console.log(" - dado1Atk = " + dado1Atk + " - dado2Atk = " + dado2Atk + " - dado3Atk = "+dado3Atk+" - dado1Def = " + dado1Def + " - dado2Def = " + dado2Def + " - dado3Def = "+ dado3Def)
+    console.log("CCC - dado1Atk = " + dado1Atk + " - dado2Atk = " + dado2Atk + " - dado3Atk = " + dado3Atk + " - dado1Def = " + dado1Def + " - dado2Def = " + dado2Def + " - dado3Def = " + dado3Def)
     if (dado1Atk > dado1Def) {
         document.getElementById("idGanador1").innerHTML = "Ataque";
     }
     else {
         document.getElementById("idGanador1").innerHTML = "Defensa";
-
+    }
+    if (dado2Atk > dado2Def) {
+        document.getElementById("idGanador2").innerHTML = "Ataque";
+    }
+    else {
+        document.getElementById("idGanador2").innerHTML = "Defensa";
+    }
+    if (dado3Atk > dado3Def) {
+        document.getElementById("idGanador3").innerHTML = "Ataque";
+    }
+    else {
+        document.getElementById("idGanador3").innerHTML = "Defensa";
     }
 }
